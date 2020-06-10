d3859a31aaaa23c9426c363ccef02c7283016b97
Anya <annagapyuk@gmail.com>
2020-06-10 09:07:44 -0400

    Added forEach and map iteration methods

---
+++ index.js
@@ -0,0 +1,34 @@
+// JS Nuggets
+// Array iteration: 8 methods
+
+// forEach
+[1,2,3].forEach(function (item, index) {
+    alert(item, index);
+});
+
+// map
+const three = [1,2,3];
+const doubled = three.map(function (item) {
+    return item * 2;
+});
+alert(doubled);
+
+
+// filter
+
+
+// reduce
+
+
+
+// some
+
+
+
+// every
+
+
+
+// find
+
+
